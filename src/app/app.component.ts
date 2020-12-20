f4c19b624b1d4729f73d2ac3339823b2f2170b68
Zsolt Endreffy <ifjezsolt@gmail.com>
2020-12-20 23:56:39 +0100

    app.component: consume url parameter api-root
    This can be used to set the api root via a HTTP
    request.
    E.g:
    http://localhost:4200/?api-root=http%3A%2F%2F127.0.0.1%3A8000%2Fmerchant_game%2Fapi%2F
    This sets the root api to:
    http://127.0.0.1:8000/merchant_game/api/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {MediaMatcher} from '@angular/cdk/layout';
 import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ApiService} from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,23 @@ export class AppComponent implements OnDestroy {
 
   private readonly mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService,
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this.mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this.mobileQueryListener);
+    this.activatedRoute.queryParams.subscribe(
+      params => {
+        let apiRoot = params['api-root'];
+        if (apiRoot) {
+          this.apiService.setApiRoot(apiRoot).subscribe();
+        }
+      }
+    );
   }
 
   ngOnDestroy(): void {
